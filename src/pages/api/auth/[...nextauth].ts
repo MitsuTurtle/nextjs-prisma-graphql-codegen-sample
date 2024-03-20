5525246b87118af89fcc1e8e667614e911143e11
Mitsuhiro Kawashima <mshimashima@gmail.com>
2024-03-20 18:20:50 +0900

    メールアドレスログインの実装

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,13 @@
-import EmailProvider from 'next-auth/providers/email'
-import NextAuth from 'next-auth'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
-import { prisma } from '@/libs/prisma'
+import NextAuth from 'next-auth'
+import EmailProvider from 'next-auth/providers/email'
+
+import prisma from '@/libs/prisma'
 
 const EMAIL_SERVER = process.env.EMAIL_SERVER
 const EMAIL_FROM = process.env.EMAIL_FROM
 const NEXTAUTH_SECRET = process.env.NEXTAUTH_SECRET
+
 
 if (!EMAIL_SERVER || !EMAIL_FROM || !NEXTAUTH_SECRET) {
   throw new Error('EMAIL_SERVER, or EMAIL_FROM, or NEXTAUTH_SECRET not found.')
